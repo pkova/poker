2c86ff38e92d0a02f031d3924167d70ca0392575
Peter Kovanen <kovanen.peter@gmail.com>
2016-09-18 23:20:11 -0700

    Add simple buttons to frontend

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      betButton: 'Raise',
+      callButton: 'Call'
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="game">
+        <div className="video"></div>
+        <div className="controls">
+          <button className="bet">{this.state.betButton}</button>
+          <button className="call">{this.state.callButton}</button>
+          <button className="fold">Fold</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
