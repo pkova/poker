7a15a6fec87c1fd9fba5138f2b6f1d802bc803dd
Peter Kovanen <kovanen.peter@gmail.com>
2016-09-19 15:16:59 -0700

    Initial setup for animation complete

--- client/main.js
+++ client/main.js
@@ -1,19 +1,50 @@
 var scene = new THREE.Scene();
-var cameraPosition = new THREE.Vector3(0, 0, 5);
 var raycaster = new THREE.Raycaster();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(cameraPosition);
+camera.position.set(0, 0, 5);
 
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.querySelector('.video').appendChild(renderer.domElement);
 
-var createCard = function(pos) {
+var createCard = function() {
   var geometry = new THREE.PlaneGeometry(1, 2);
-  var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-  material.side = THREE.Doubleside;
+  var material = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.DoubleSide});
   var mesh = new THREE.Mesh(geometry, material);
+  mesh.rotation.set(2, 0, 0);
   scene.add(mesh);
   return mesh;
 };
+
+var card1 = createCard();
+var card2 = createCard();
+
+var dealStartingCards = function() {
+  var coords = { x: 1, y: 2 };
+  var tween = new TWEEN.Tween(coords)
+      .to({ x: -0.5, y: -1 }, 1100)
+      .start();
+
+  var coords2 = { x: 1, y: 2 };
+  var tween2 = new TWEEN.Tween(coords2)
+    .to({ x: 0.5, y: -1 }, 1100)
+    .start();
+
+  tween.onUpdate( function() {
+    card1.position.set(coords.x, coords.y, 0);
+    card1.rotation.set(card1.rotation.x, card1.rotation.y, card1.rotation.z + 0.1);
+
+    card2.position.set(coords2.x, coords2.y, 0);
+    card2.rotation.set(card2.rotation.x, card2.rotation.y, card2.rotation.z + 0.1);
+  });
+};
+
+dealStartingCards();
+
+function render() {
+  TWEEN.update();
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+render();
