a3dd26a43f5612aa0e95173ab065c199a7f86784
Peter Kovanen <kovanen.peter@gmail.com>
2016-10-02 01:08:18 -0700

    More work on clojure poker engine

--- lib/poker.clj
+++ lib/poker.clj
@@ -55,37 +55,58 @@
             "2C"
             ])
 
-(defn validate-min-players [n]
-  (if (< n 2)
+(defn validate-min-players [table]
+  (if (< (table :min-players) 2)
     [nil "Parameter min-players must be a positive integer of a minimum value of 2"]
-    [n nil]))
+    [table nil]))
 
-(defn validate-max-players [n]
-  (if (> n 10)
+(defn validate-max-players [table]
+  (if (> (table :max-players) 10)
     [nil "Parameter max-players must be a positive integer less than or equal to 10"]
-    [n nil]))
+    [table nil]))
 
-(defn validate-min-max-players [min max]
-  (if (> min max)
+(defn validate-min-max-players [table]
+  (if (> (table :min-players) (table :max-players))
     [nil "Parameter min-players must be a positive integer less than or equal to max-players"]
-    [[min max] nil]))
+    [table nil]))
 
-(defn table [sb bb min-players max-players min-buyin max-buyin]
+(defn bind-error [f [val err]]
+  (if (nil? err)
+    (f val)
+    [nil err]))
+
+(defmacro err->> [val & fns]
+  (let [fns (for [f fns] `(bind-error ~f))]
+    `(->> [~val nil]
+          ~@fns)))
+
+(defn validate [table]
+  (err->> table
+          validate-min-players
+          validate-max-players
+          validate-min-max-players))
+
+(defn table
   "Returns a map representing a new poker table"
-  {:small-blind sb,
-   :big-blind bb,
-   :max-players max-players,
-   :players [],
-   :current-player {},
-   :dealer {},
-   :min-buyin min-buyin,
-   :max-buyin max-buyin,
-   :players-to-remove [],
-   :players-to-add [],
-   :turn-bet {},
-   :game {},
-   :game-winners [],
-   :game-losers []})
+  [sb bb min-players max-players min-buyin max-buyin]
+  (let [t {:small-blind sb,
+           :big-blind bb,
+           :min-players min-players,
+           :max-players max-players,
+           :players [],
+           :current-player {},
+           :dealer {},
+           :min-buyin min-buyin,
+           :max-buyin max-buyin,
+           :players-to-remove [],
+           :players-to-add [],
+           :turn-bet {},
+           :game {},
+           :game-winners [],
+           :game-losers []}
+
+        ]
+    (validate t)))
 
 (defn player [name chips]
   "Returns a map representing a player"
@@ -113,3 +134,6 @@
 
 (defn bankrupt-players [table]
   (filter #(= (% :chips) 0) (table :players)))
+
+(defn rank-hands [hands]
+  "Return a vector of sorted poker hands, index 0 contains winner(s)")
